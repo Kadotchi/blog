ce193f5d2c266b320d57dfb22a716a70efd4ff0c
Kadotchi <kadotchi724@gmail.com>
2023-07-04 22:06:56 +0900

    ✨ カスタムAppコンポートでLayoutを全ページに適用

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,7 @@
-import Footer from 'components/footer'
-import Header from 'components/header'
 import Hero from 'components/hero'
 
 export default function Home() {
   return (
-    <>
-      <Header />
-
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
+    <Hero />
   )
 }
