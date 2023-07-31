a22cb0ef71af62b9e49e00da4176e435b54d2fb2
Kadotchi <kadotchi724@gmail.com>
2023-08-01 07:32:03 +0900

    ✨ ソーシャルリンクメニューを作成

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,6 +1,7 @@
 import Container from 'components/Container'
 import Logo from 'components/logo'
 import styles from 'styles/Footer.module.css'
+import Social from 'components/Social'
 
 export default function Footer() {
   return (
@@ -8,7 +9,7 @@ export default function Footer() {
       <Container>
         <div className={styles.flexContainer}>
           <Logo />
-          [ソーシャル]
+          <Social />
         </div>
       </Container>
     </footer>
