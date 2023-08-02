5ba5a1e69ac3aebf870502ad47ee29238adedbeb
Kadotchi <kadotchi724@gmail.com>
2023-08-03 07:52:26 +0900

    ✨ サイトに関する情報を共有できるようにする

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,12 @@
 import Container from 'components/Container'
 import Hero from 'components/Hero'
+import Meta from 'components/Meta'
 
 export default function Blog() {
   return (
     <Container>
+      <Meta pageTitle={'ブログ'} />
+
       <Hero title="Blog" subtitle="Recent Posts" />
     </Container>
   )
