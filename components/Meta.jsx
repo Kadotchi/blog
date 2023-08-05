cfd9d0e37e2b6aff3ec370eac150d2245b77de40
Kadotchi <kadotchi724@gmail.com>
2023-08-05 14:05:39 +0900

    ✨ ページのURLを追加

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 // サイトに関する情報
 import { siteMeta } from 'lib/constants'
@@ -12,6 +13,10 @@ export default function Meta({ pageTitle, pageDesc }) {
   // ページの説明
   const desc = pageDesc ?? siteDesc
 
+  // ページのURL
+  const router = useRouter()
+  const url = `${siteUrl}${router.asPath}`
+
   return (
     <Head>
       <title>{title}</title>
@@ -19,6 +24,9 @@ export default function Meta({ pageTitle, pageDesc }) {
 
       <meta property="description" content={desc} />
       <meta property="og:description" content={desc} />
+
+      <link rel="canonical" href={url} />
+      <meta property="og:url" content={url} />
     </Head>
   )
 }
