f67194ef380a7fdc396da9e56a4616158bb68ae8
Kadotchi <kadotchi724@gmail.com>
2023-08-16 06:29:32 +0900

    ✨ 全ての記事データを取得

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,8 +1,10 @@
+import { getAllPosts } from 'lib/api'
 import Container from 'components/Container'
 import Hero from 'components/Hero'
 import Meta from 'components/Meta'
 
-export default function Blog() {
+export default function Blog({ posts }) {
+  console.log(posts)
   return (
     <Container>
       <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
@@ -10,4 +12,10 @@ export default function Blog() {
       <Hero title="Blog" subtitle="Recent Posts" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts()
+
+  return { props: { posts: posts } }
 }
