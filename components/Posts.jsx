cba3217591af4097c5120ec87ef7e54b16272bb0
Kadotchi <kadotchi724@gmail.com>
2023-08-18 00:41:46 +0900

    ✨ 記事一覧の表示

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,3 +1,16 @@
-export default function Posts() {
-  return <div>Posts</div>
+import styles from 'styles/Posts.module.css'
+import Link from 'next/link'
+
+export default function Posts({ posts }) {
+  return (
+    <div className={styles.gridContainer}>
+      {posts.map(({ title, slug }) => (
+        <article className={styles.posts} key={slug}>
+          <Link href={`slug/${slug}`}>
+            <h2>{title}</h2>
+          </Link>
+        </article>
+      ))}
+    </div>
+  )
 }
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -2,14 +2,16 @@ import { getAllPosts } from 'lib/api'
 import Container from 'components/Container'
 import Hero from 'components/Hero'
 import Meta from 'components/Meta'
+import Posts from 'components/Posts'
 
 export default function Blog({ posts }) {
-  console.log(posts)
   return (
     <Container>
       <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
 
       <Hero title="Blog" subtitle="Recent Posts" />
+
+      <Posts posts={posts} />
     </Container>
   )
 }
