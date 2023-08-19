5951d66fa1f1fd34bfd3399c6fe345ebbc8aa946
Kadotchi <kadotchi724@gmail.com>
2023-08-19 11:18:54 +0900

    ✨ 記事一覧のレイアウトを整える

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,4 +1,8 @@
+import { getPlaiceholder } from 'plaiceholder'
+
 import { getAllPosts } from 'lib/api'
+import { eyecatchLocal } from 'lib/constants'
+import { getImageBuffer } from 'lib/getImageBuffer'
 import Container from 'components/Container'
 import Hero from 'components/Hero'
 import Meta from 'components/Meta'
@@ -18,6 +22,15 @@ export default function Blog({ posts }) {
 
 export async function getStaticProps() {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return { props: { posts: posts } }
 }
