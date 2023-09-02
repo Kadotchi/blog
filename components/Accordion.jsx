db2d0020dc96850cefa269398d526d22f26896ea
Kadotchi <kadotchi724@gmail.com>
2023-09-02 13:17:12 +0900

    ✨ アコーディオンを開いた状態を作成

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -1,5 +1,24 @@
-import React from 'react'
+import styles from 'styles/Accordion.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons'
 
-export default function Accordion() {
-  return <div>Accordion</div>
+/**
+ * アコーディオンコンポーネント
+ * @param {heading} 見出し
+ * @param {children} テキスト
+ * @returns {JSX.Element}
+ */
+export default function Accordion({ heading, children }) {
+  return (
+    <div className={styles.open}>
+      <h3 className={styles.heading}>
+        <button>
+          {heading}
+          <FontAwesomeIcon icon={faCircleArrowDown} className={styles.icon} />
+        </button>
+      </h3>
+      <div className={styles.text}></div>
+      <div className={styles.textInner}>{children}</div>
+    </div>
+  )
 }
